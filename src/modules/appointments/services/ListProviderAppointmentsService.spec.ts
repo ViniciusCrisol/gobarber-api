86c4feb48727803799b1e8485be597a0f1815bce
ViníciusCrisol <vinicius.crisol@hotmail.com>
2020-08-26 22:44:33 -0300

    Appointments list

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,38 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let listProviderAppointments: ListProviderAppointmentsService;
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list appointments on a specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      user_id: '9719f904-ed53-4d2b-8729-87533',
+      provider_id: '9iuj9shh-ed53-422c-8722-88433',
+      date: new Date(2020, 4, 1, 14, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      user_id: '9719f904-ed53-4d2b-8729-87533',
+      provider_id: '9iuj9shh-ed53-422c-8722-88433',
+      date: new Date(2020, 4, 1, 16, 0, 0),
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: '9iuj9shh-ed53-422c-8722-88433',
+      year: 2020,
+      month: 5,
+      day: 1,
+    });
+
+    expect(appointments).toEqual([appointment1, appointment2]);
+  });
+});
